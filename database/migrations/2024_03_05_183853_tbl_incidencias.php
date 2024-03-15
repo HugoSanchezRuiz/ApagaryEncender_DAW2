84659a3daf37b2a3df317dc0af2e11897ef4e35d
iker catalan gomez <100007043.joan23@fje.edu>
2024-03-15 17:23:21 +0100

    Seeder tbl_subcategoria + campo imagen y descripción en tbl_incidencias

--- database/migrations/2024_03_05_183853_tbl_incidencias.php
+++ database/migrations/2024_03_05_183853_tbl_incidencias.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('id_tecnico');
             $table->unsignedBigInteger('id_cliente');
             $table->unsignedBigInteger('id_subcategoria');
+            $table->text('imagen');
+            $table->text('descripción');
             $table->enum('estado', ['Sin asignar', 'Asignada', 'En proceso', 'Resuelta', 'Cerrada'])->default('Sin asignar');
             $table->timestamps();
 
